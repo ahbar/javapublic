c4c89e3794a3fcfecdbc1863bd7c6243275964bb
Jeff <jeff.mk7@gmail.com>
2014-10-25 01:42:02 -0700

    23f

--- utilsmk44/src/utilsmk44/Test.java
+++ utilsmk44/src/utilsmk44/Test.java
@@ -2,10 +2,10 @@ package utilsmk44;
 
 public class Test {
 
+	// testing of utils programs
 	public static void main(String[] args) 
 	{
-		System.out.println("sdflkjsdf");
-		
+		System.out.println("test utils started");
 	}
 
 }
