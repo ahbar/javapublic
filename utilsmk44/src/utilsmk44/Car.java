01505222593c4d2fee2ddeccfbb5ac4ffe15a969
Jeff <jeff.mk7@gmail.com>
2014-10-26 22:28:13 -0700

    2wef

---
+++ utilsmk44/src/utilsmk44/Car.java
@@ -0,0 +1,9 @@
+package utilsmk44;
+
+/**
+ * Created by Jeff on 10/26/2014.
+ */
+class Car
+{
+    public String name;
+}
--- utilsmk44/src/utilsmk44/Test.java
+++ utilsmk44/src/utilsmk44/Test.java
@@ -1,11 +1,14 @@
 package utilsmk44;
 
-public class Test {
-
-	// testing of utils programs
-	public static void main(String[] args) 
+// testing of utils programs
+public class Test 
+{
+	public static void main(String[] args)
 	{
 		System.out.println("test utils started");
+        for (int i=0; i<22; i++)
+        {
+            System.out.println(i);
+        }
 	}
-
 }
