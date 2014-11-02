be2ab4b260f123b7af06099f20d644d364658ef7
Jeff <jeff.mk7@gmail.com>
2014-11-01 17:20:26 -0700

    w3r

--- utilsmk44/src/utilsmk44/Test.java
+++ utilsmk44/src/utilsmk44/Test.java
@@ -1,14 +1,15 @@
 package utilsmk44;
+
+import Drill1.Drill1;
+
 //testing github
 // testing of utils programs
 public class Test 
 {
 	public static void main(String[] args)
 	{
-		System.out.println("test utils started");
-        for (int i=0; i<22; i++)
-        {
-            System.out.println(i);
-        }
+        Drill1 boris = new Drill1();
+        boris.outputMessage();
+
 	}
 }
