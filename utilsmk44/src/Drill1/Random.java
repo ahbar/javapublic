9ededacbacfa7ff2906f7048c4224985cf52487b
borishazanov <boris.hazanov@gmail.com>
2014-11-02 22:36:55 -0500

    HW1

---
+++ utilsmk44/src/Drill1/Random.java
@@ -0,0 +1,36 @@
+package Drill1;
+import java.util.Random;
+/**
+ * Created by boris on 11/2/2014.
+ */
+class random
+{
+    public static void main(String args[])
+    {
+        int countLessThenTen = 0;
+        Random rn = new Random();
+        int[] randomArray = new int[100];
+
+        for(int i= 0;i<40;i++ )
+        {
+            randomArray[i] = rn.nextInt(100);
+            System.out.println(randomArray[i]);
+            if(randomArray[i]<=10)
+            {
+                countLessThenTen++;
+            }
+
+        }
+        System.out.println("there were " +countLessThenTen);
+
+    }
+
+ }
+
+
+
+
+
+
+
+
--- utilsmk44/src/utilsmk44/Test.java
+++ utilsmk44/src/utilsmk44/Test.java
@@ -1,22 +1,16 @@
 package utilsmk44;
 
-<<<<<<< HEAD
 
-=======
+
 import Drill1.Drill1;
->>>>>>> origin/master
 
-//testing github
-// testing of utils programs
 public class Test 
 {
 	public static void main(String[] args)
 	{
-<<<<<<< HEAD
-=======
+
         Drill1 boris = new Drill1();
         boris.outputMessage();
->>>>>>> origin/master
 
 	}
 }
