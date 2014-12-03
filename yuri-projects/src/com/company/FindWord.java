e830f7d6bae544b5b994e0e1d1a414fde82700b8
yuri yudashkin <yuri.yudashkin@gmail.com>
2014-12-02 17:29:18 -0700

    Final Version

--- yuri-projects/src/com/company/FindWord.java
+++ yuri-projects/src/com/company/FindWord.java
@@ -10,31 +10,34 @@ package com.company;
  */
 public class FindWord {
     String myText= "";
-    int len;
-    public void calculateLength ()
+   // int len;
+   /* public void calculateLength ()
     {
 
         len = myText.length();
 
-    }
+    }*/
 
     public int finalNumber (String wantedWord)
     {
 
         String[] currentWord = myText.split(" ");
+        int wordCount = currentWord.length;
+        System.out.println("number of words:" +wordCount);
+
         System.out.println("The word im looking for is:");
         System.out.println(wantedWord);
         int counter=0;
 
-        for (int i = 0; i<len; i++)
+        for (int i = 0; i<wordCount; i++)
         {
             if (currentWord[i].contains(wantedWord))
               counter++;
-                else
-                System.out.println(counter);
+               // else
+               // System.out.println(counter);
         }
-        System.out.println("Wanted word was found" +counter);
-        return 0;
+        System.out.println("Wanted word was found " +counter+ " times");
+        return counter;
     }
 
 
--- yuri-projects/src/com/company/Main.java
+++ yuri-projects/src/com/company/Main.java
@@ -2,20 +2,26 @@ package com.company;
 import java.io.*;
 import java.io.IOException;
 import java.lang.String;
+import java.util.Scanner;
+
 import static java.lang.System.out;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        String a ="";
+        Scanner in = new Scanner(System.in);
         StringOperator stringOperator = new StringOperator();
         FindWord findWord = new FindWord();
         findWord.myText =  stringOperator.readFile("C:/TestData/text.txt");
         System.out.println("This is the text:");
         System.out.println(findWord.myText);
-        findWord.calculateLength();
-        System.out.println("Length of text is:");
-        System.out.println(findWord.len);
+       // findWord.calculateLength();
+       // System.out.println("Length of text is:");
+       // System.out.println(findWord.len);
         //System.out.println(currentWord[1]);
-        findWord.finalNumber("a");
+        System.out.println("What word are you looking for?");
+        a = in.nextLine();
+        findWord.finalNumber(a);
 
     }
 }
